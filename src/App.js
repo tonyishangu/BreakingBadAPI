7fb0a39f14e6a8ba8a015178f3526556eabe3710
Tonnie Ishangu <67873141+tonyishangu@users.noreply.github.com>
2021-03-18 08:40:14 +0300

    trial in consuming api via axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
+import Headers from './components/ui/Headers'
+import Search from './components/ui/Search'
+import CharacterGrid from './components/characters/CharacterGrid'
 
 function App() {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect (() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      setItems(result.data)
+      setIsLoading(false)
+      console.log(result.data)
+    }
+    fetchItems()
+  },[query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Headers />
+      < Search getQuery={(q)=>setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
---
+++ src/components/ui/Headers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+
+const Headers = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt=""/>
+        </header>
+    )
+}
+
+export default Headers
